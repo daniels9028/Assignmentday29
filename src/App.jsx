a530d97a07cbc7dcafde2b6c7cb8124c0a699c03
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-04 11:20:33 +0700

    Services Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,67 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Services from "./components/Services";
 
 import banner from "./assets/banner.png";
+
+import services1 from "./assets/services1.png";
+import services2 from "./assets/services2.png";
+import services3 from "./assets/services3.png";
+import services4 from "./assets/services4.png";
+import services5 from "./assets/services5.png";
+import services6 from "./assets/services6.png";
+
+const services = [
+  {
+    id: 1,
+    title: "Search Doctor",
+    description:
+      "Choose your doctor from thousands of specialist, general, and trusted hospitals",
+    icon: services1,
+  },
+  {
+    id: 2,
+    title: "Online Pharmacy",
+    description:
+      "Buy your medicines with our mobile application with a simple delivery system",
+    icon: services2,
+  },
+  {
+    id: 3,
+    title: "Consultation",
+    description:
+      "Free consultation with our trusted doctors and get the best recomendations",
+    icon: services3,
+  },
+  {
+    id: 4,
+    title: "Details Info",
+    description:
+      "Free consultation with our trusted doctors and get the best recomendations",
+    icon: services4,
+  },
+  {
+    id: 5,
+    title: "Emergency Care",
+    description:
+      "You can get 24/7 urgent care for yourself or your children and your lovely family",
+    icon: services5,
+  },
+  {
+    id: 6,
+    title: "Tracking",
+    description: "Track and save your medical history and health data",
+    icon: services6,
+  },
+];
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Banner banner={banner} />
+      <Services services={services} />
     </>
   );
 };
---
+++ src/components/CardServices.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CardServices = ({ service }) => {
+  return (
+    <div className="w-[250px] h-[250px] bg-white rounded-xl shadow-md p-6 space-y-4">
+      <img
+        src={service.icon}
+        alt={service.title}
+        className="w-10 h-10 bg-contain"
+      />
+      <h4 className="font-semibold text-lg">{service.title}</h4>
+      <p className="text-[#7D7987] text-sm">{service.description}</p>
+    </div>
+  );
+};
+
+export default CardServices;
